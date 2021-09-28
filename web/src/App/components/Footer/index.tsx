e9c6cfa13916d1486b1a5540ee33db3440a968b0
deltd3v <deltd3v@gmail.com>
2021-09-28 16:02:32 +0300

    ⭐😎 added footer

--- web/src/App/components/Footer/index.tsx
+++ web/src/App/components/Footer/index.tsx
@@ -1,21 +1,38 @@
-import { Component } from 'solid-js';
+import { Component, For } from 'solid-js';
 import Logo from '../Logo';
 
 import s from './styles.module.scss';
+
+const FooterLink: Component<{
+	linkGroupTitle: string;
+	links: { link: string; label: string }[];
+}> = (props) => {
+	return (
+		<div class={s.linkItems}>
+			<h3 class={s.linkTitle}>{props.linkGroupTitle}</h3>
+
+			<For
+				each={props.links}
+				fallback={<div>Links are being loaded...</div>}>
+				{(l, _index) => (
+					<a href={l.link} class={s.link}>
+						{l.label}
+					</a>
+				)}
+			</For>
+		</div>
+	);
+};
 
 const FooterSection: Component = () => {
 	return (
 		<div id="footer" class={s.container}>
 			<div class={s.subscription}>
-                <section class={s.subHeading}>
-                    <pre class="margin: .6em 0em">
-
-                    Wanna know what we're upto ?
-                    </pre>
-                    <h3>
-
-                    Great ! ⭐Subscribe to our weekly newsletter 📰
-                    </h3>
+				<section class={s.subHeading}>
+					<pre class="margin: .6em 0em">
+						Wanna know what we're upto ?
+					</pre>
+					<h3>Great ! ⭐Subscribe to our weekly newsletter 📰</h3>
 				</section>
 				<p class={s.subText}>You can cancel it any time</p>
 				<form action="#" class={s.form}>
@@ -32,150 +49,106 @@ const FooterSection: Component = () => {
 
 			<div class={s.linksContainer}>
 				<div class={s.linksWrapper}>
-					<div class={s.linkItems}>
-						<h2 class={s.linkTitle}>About</h2>
-						<a href="#" class={s.link}>
-							Info
-						</a>
-						<a href="#" class={s.link}>
-							Careers
-						</a>
-						<a href="#" class={s.link}>
-							Stocks
-						</a>
-						<a href="#" class={s.link}>
-							Investors
-						</a>
-						<a href="#" class={s.link}>
-							Terms and Conditions
-						</a>
-					</div>
+					<FooterLink
+						linkGroupTitle="Info"
+						links={[
+							{ label: 'Careers', link: '#' },
+							{ label: 'Stocks', link: '#' },
+							{ label: 'Investors', link: '#' },
+							{ label: 'Terms and Codes', link: '#' },
+						]}></FooterLink>
 
-					<div class={s.linkItems}>
-						<h2 class={s.linkTitle}>Contacts</h2>
-						<a href="#" class={s.link}>
-							Post
-						</a>
-						<a href="#" class={s.link}>
-							Mail
-						</a>
-						<a href="#" class={s.link}>
-							Chat
-						</a>
-						<a href="#" class={s.link}>
-							Phone
-						</a>
-						<a href="#" class={s.link}>
-							Bird Carriers 🦅
-						</a>
-					</div>
+					<FooterLink
+						linkGroupTitle="Contacts"
+						links={[
+							{ label: 'Mail', link: '#' },
+							{ label: 'Chat', link: '#' },
+							{ label: 'Phone', link: '#' },
+							{ label: 'Bird Carriers 🦅', link: '#' },
+						]}></FooterLink>
 
-					<div class={s.linkItems}>
-						<h2 class={s.linkTitle}>Operations</h2>
-						<a href="#" class={s.link}>
-							Headquarters
-						</a>
-						<a href="#" class={s.link}>
-							Branches
-						</a>
-						<a href="#" class={s.link}>
-							Managers
-						</a>
-						<a href="#" class={s.link}>
-							Invistors
-						</a>
-						<a href="#" class={s.link}>
-							Customer Relations
-						</a>
-					</div>
+					<FooterLink
+						linkGroupTitle="Headquarters"
+						links={[
+							{ label: 'Branches', link: '#' },
+							{ label: 'Managers', link: '#' },
+							{ label: 'Investors', link: '#' },
+							{ label: 'Customer Relations', link: '#' },
+						]}></FooterLink>
 
-					 <div class={s.linkItems}>
-						<h2 class={s.linkTitle}>Platforms</h2>
-						<a href="#" class={s.link}>
-                            Code app
-                        </a>
-						<a href="#" class={s.link}>
-							ATMs
-						</a>
-						<a href="#" class={s.link}>
-							Mobile
-						</a>
-						<a href="#" class={s.link}>
-							Web
-						</a>
-						<a href="#" class={s.link}>
-							Cards
-						</a>
-					</div>  
+					<FooterLink
+						linkGroupTitle="Platforms"
+						links={[
+							{ label: 'Mobile', link: '#' },
+							{ label: 'Web', link: '#' },
+							{ label: 'ATMs', link: '#' },
+							{ label: 'Code app', link: '#' },
+						]}></FooterLink>
 				</div>
-				<div class={s.socialMediaSection}>
-					<section class={s.socialMediaWrap}>
-						<a
-							href="#"
-							class={s.socialMediaLogo}
-							style="font-size: 1.2em; align-items:center; font-weight: bolder;">
-							star
-							<Logo
-								class={s.socialMediaIcon}
-								width={50}
-								height={30}
-								strokeColor={'white'}
-								strokeWidth={80}></Logo>
-							bank
-						</a>
-					</section>
+			</div>
+			<div class={s.socialMediaSection}>
+				<section class={s.socialMediaWrap}>
+					<a
+						href="#"
+						class={s.socialMediaLogo}
+						style="font-size: 1.2em; align-items:center; font-weight: bolder;">
+						star
+						<Logo
+							class={s.socialMediaIcon}
+							width={50}
+							height={30}
+							strokeColor={'white'}
+							strokeWidth={80}></Logo>
+						bank
+					</a>
+				</section>
 
-					<code class={s.copyRight}style="margin: 0 .5em">
-						 © {new Date().getFullYear()}
-					</code>
+				<div class={s.copyRight}>
+					© <pre>{new Date().getFullYear()}</pre>
+				</div>
 
-					<div class={s.socialMediaIcons}>
-						<a
-							class={s.socialMediaIconLink}
-							href="#"
-							target="_blank"
-							aria-label="Facebook">
-							<i
-								class={s.socialMediaIcon}
-								data-feather="facebook"></i>
-						</a>
-						<a
-							class={s.socialMediaIconLink}
-							href="#"
-							target="_blank"
-							aria-label="Instagram">
-							<i
-								class={s.socialMediaIcon}
-								data-feather="instagram"></i>
-						</a>
-						<a
-							class={s.socialMediaIconLink}
-							href="#"
-							target="_blank"
-							aria-label="Youtube">
-							<i
-								class={s.socialMediaIcon}
-								data-feather="youtube"></i>
-						</a>
-						<a
-							class={s.socialMediaIconLink}
-							href="#"
-							target="_blank"
-							aria-label="Twitter">
-							<i
-								class={s.socialMediaIcon}
-								data-feather="twitter"></i>
-						</a>
-						<a
-							class={s.socialMediaIconLink}
-							href="#"
-							target="_blank"
-							aria-label="LinkedIN">
-							<i
-								class={s.socialMediaIcon}
-								data-feather="linkedin"></i>
-						</a>
-					</div>
+				<div class={s.socialMediaIcons}>
+					<a
+						class={s.socialMediaIconLink}
+						href="#"
+						target="_blank"
+						aria-label="Facebook">
+						<i
+							class={s.socialMediaIcon}
+							data-feather="facebook"></i>
+					</a>
+					<a
+						class={s.socialMediaIconLink}
+						href="#"
+						target="_blank"
+						aria-label="Instagram">
+						<i
+							class={s.socialMediaIcon}
+							data-feather="instagram"></i>
+					</a>
+					<a
+						class={s.socialMediaIconLink}
+						href="#"
+						target="_blank"
+						aria-label="Youtube">
+						<i class={s.socialMediaIcon} data-feather="youtube"></i>
+					</a>
+					<a
+						class={s.socialMediaIconLink}
+						href="#"
+						target="_blank"
+						aria-label="Twitter">
+						<i class={s.socialMediaIcon} data-feather="twitter"></i>
+					</a>
+					<a
+						class={s.socialMediaIconLink}
+						href="#"
+						target="_blank"
+						aria-label="LinkedIN">
+						<i
+							class={s.socialMediaIcon}
+							data-feather="linkedin"></i>
+					</a>
 				</div>
 			</div>
 		</div>
